597de0ea4569eb6735041f0d83ed2ff5e1e9a268
Sourav Kumar Saha <souravsaha.prgmr@gmail.com>
2024-07-07 00:43:11 +0600

    function with two parameter to generate 16 character string implementet

--- index.js
+++ index.js
@@ -1,6 +1,52 @@
 
-function word(data){
-    return data+" isshan";
+//generate 16 character random number
+function generate(size=16){
+
+    hex=''
+    while(hex.length <size){
+        hex+=Math.floor(Math.random()*16).toString(16)
+    }
+    return hex
 }
 
-module.exports=word
+function hexBetween(start='',end=''){
+
+    if (!validHex(start)  || !validHex(end)){
+        throw new error('invalid hexadecimal number')
+    }else{
+
+        start=parseInt(start,16)
+        end=parseInt(end,16)
+        console.log(start + " " + end)
+        var max=0;
+        var min=0;
+
+        if(start>end){
+            max=start
+            min=end
+        }else{
+            max=end
+            min=start
+        }
+
+        hex=''
+        while(hex.length<16){
+
+            hex += Math.floor(Math.random() * (max-min+1)+min).toString(16)
+
+        }
+        return hex
+
+    }
+
+
+    function validHex(char){
+        return /^[0-9a-fA-F]$/.test(char);
+    }
+
+}
+
+module.exports={
+    generate,
+    hexBetween
+}
